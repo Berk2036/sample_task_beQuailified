8bab51901ad2fa3175b7b763778e4dc28bc60de6
Fatih Simsek <fatih.simsek@emm-it.de>
2024-11-01 18:26:31 +0100

    Login page added

---
+++ src/test/java/beQualified/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package beQualified.pages;
+
+
+import beQualified.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    //for username
+    @FindBy(id = "user-name")
+    public WebElement usernameField;
+
+
+    //for password
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    //Login button
+    @FindBy(id = "login-button")
+    public WebElement loginButton;
+
+    //for error messages
+    @FindBy(className = "error-message-container")
+    public WebElement errorMessage;
+
+
+    public void login(String username, String password) {
+
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+
+    }
+
+    public void login(String password ) {
+
+        passwordField.sendKeys(password);
+        loginButton.click();
+
+    }
+
+    public void loginWithEmptyPassword(String username) {
+
+        usernameField.sendKeys(username);
+        loginButton.click();
+
+    }
+
+
+
+
+
+}
