d2df8d0d034cbdce3cd63f39d0463b1eeb53abb3
Fatih Simsek <fatih.simsek@emm-it.de>
2024-11-04 23:00:41 +0100

    Driver  added

---
+++ src/test/java/beQualified/utilities/utilities/Driver.java
@@ -0,0 +1,86 @@
+package beQualified.utilities.utilities;
+
+import beQualified.utilities.ConfigurationReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    //create a private constructor to remove access to this object
+    private Driver() {
+    }
+
+    /*
+   static, because it will be used in a static method.
+     */
+    //private static WebDriver driver; // default value = null
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    /*
+    Create a re-usable utility method which will return the same driver instance once we call it.
+    - If an instance doesn't exist, it will create first, and then it will always return same instance.
+     */
+    public static WebDriver getDriver() {
+
+        if (driverPool.get() == null) {
+
+            /*
+            To browserType from configuration.properties file.
+            To have a control which browser is opened from outside our code.
+             */
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            /*
+            Depending on the browserType returned from the configuration.properties
+            switch statement will determine the "case", and open the matching browser.
+             */
+            switch (browserType) {
+                case "chrome":
+                    //WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--ignore-certificate-errors","--disable-search-engine-choice-screen");
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                case "firefox":
+                    //WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                case "chromeThirdParty":
+                    String pathForChromeDriver = "/src/test/resources/webdriver/chromedriver127.exe";
+                    System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + pathForChromeDriver);
+                    driverPool.set(new ChromeDriver());
+                default:
+            }
+
+        }
+
+        return driverPool.get();
+
+    }
+
+    /*
+    Create a new Driver.closeDriver(); it will use .quit() method to quit browsers, and then set the driver value back to null.
+     */
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            /*
+            This line will terminate the currently existing driver completely. It will not exist going forward.
+             */
+            driverPool.get().quit();
+            /*
+            assigned the value back to "null" so that my "singleton" can create a newer one if needed.
+             */
+            driverPool.remove();
+        }
+    }
+
+}
